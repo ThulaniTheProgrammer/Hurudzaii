96ca73f24a0521f8eb640ecf41e3a1791a7860f5
ThulaniTheProgrammer <frankmakeba0@gmail.com>
2024-12-17 12:21:58 +0200

    added shona lamguages

--- src/pages/voice/voice.js
+++ src/pages/voice/voice.js
@@ -125,7 +125,7 @@ const Voice = () => {
                   type="button"
                   className="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                 >
-                  Hindi
+                 Shona
                 </button>
               </div>
               <div className="ml-16 ">
@@ -134,7 +134,7 @@ const Voice = () => {
                   type="button"
                   className="inline-block px-6 py-2.5 bg-green-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                 >
-                  Spanish
+                  Ndebele
                 </button>
               </div>
             </div>
